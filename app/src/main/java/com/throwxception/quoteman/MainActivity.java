4c2b9c886c26610178e30357bbc72ea85c253ed2
Surya Poojary <77871514+Gh0st-sniper@users.noreply.github.com>
2024-02-01 20:18:23 +0530

    Added 4 pictures and Quote class array

--- app/src/main/java/com/throwxception/quoteman/MainActivity.java
+++ app/src/main/java/com/throwxception/quoteman/MainActivity.java
@@ -11,6 +11,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Quote[] quoteof = {
+
+                new Quote("Elon Musk","When something is important enough, you do it even if the odds are not in your favor.",R.drawable.elon ),
+                new Quote("Steve Jobs","The people who are crazy enough to think they can change the world are the ones who do.",R.drawable.jobs),
+                new Quote("Naval Ravikant","You have one life. You’re dead for tens of billions of years, and you’re going to be dead for tens of billions of years.",R.drawable.naval),
+                new Quote("Joseph Nguyen","The less thinking we have, the more space we create for positive emotions to naturally surface.",R.drawable.joseph)
+
+
+        };
+
 
     }
 }
