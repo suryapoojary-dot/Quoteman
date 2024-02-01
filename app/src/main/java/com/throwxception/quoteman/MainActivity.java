7914648ad4f43219c8adc9a29f88264b68dbc75c
Surya Poojary <77871514+Gh0st-sniper@users.noreply.github.com>
2024-02-01 22:15:34 +0530

    Completed QuoteAdapter and MainActivity

--- app/src/main/java/com/throwxception/quoteman/MainActivity.java
+++ app/src/main/java/com/throwxception/quoteman/MainActivity.java
@@ -24,6 +24,9 @@ public class MainActivity extends AppCompatActivity {
 
         };
 
+        QuoteAdapter adapter = new QuoteAdapter(quoteof);
+        thelist.setAdapter(adapter);
+
 
     }
 }
--- app/src/main/java/com/throwxception/quoteman/QuoteAdapter.java
+++ app/src/main/java/com/throwxception/quoteman/QuoteAdapter.java
@@ -1,34 +1,62 @@
 package com.throwxception.quoteman;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class QuoteAdapter extends RecyclerView.Adapter<QuoteAdapter.QuoteViewHolder> {
 
+    private Quote[] quotey;
+
+    QuoteAdapter(Quote[] quotes) {
+        this.quotey = quotes;
+    }
+
+    @Override
+    public int getItemCount() {
+        return quotey.length;
+    }
 
     @NonNull
     @Override
     public QuoteViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        //return null;
+       View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_project,parent,false);
+        return new QuoteViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull QuoteViewHolder holder, int position) {
 
+        holder.bind(quotey[position]);
+
     }
 
-    @Override
-    public int getItemCount() {
-        return 0;
-    }
+
 
     static class QuoteViewHolder extends RecyclerView.ViewHolder {
 
+        ImageView appImage;
+        TextView appTitle;
+        TextView appQuote;
        public QuoteViewHolder(@NonNull View itemView) {
            super(itemView);
+           appImage = itemView.findViewById(R.id.image_view_person_logo);
+           appTitle = itemView.findViewById(R.id.text_view_person_name);
+           appQuote = itemView.findViewById(R.id.text_view_person_said);
+       }
+
+       public  void bind(Quote quote){
+
+           appTitle.setText(quote.name);
+           appQuote.setText(quote.the_quote);
+           appImage.setImageResource(quote.image);
+
        }
    }
 
